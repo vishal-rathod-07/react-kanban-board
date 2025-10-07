99e2dce574772c0bdda444b6b1d1d23c783e7d74
Sujal Shah <sujal@zuru.com>
2025-10-07 20:39:25 +0530

    chore: update dependencies and improve code organization
    - Update bootstrap to v5.3.8 and web-vitals to v5.1.0
    - Use named import for StrictMode in index.js
    - Reorder imports in App.js for better readability

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 
 const root = createRoot(document.getElementById('root'));
 
 root.render(
-  <React.StrictMode>
+  <StrictMode>
     <App />
-  </React.StrictMode>
+  </StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
